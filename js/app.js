2488d441f7ab9ea834ac96da98a7316d8c4c166b
Pedro <doctorfast10.12@hotmail.com>
2024-07-03 21:28:55 -0300

    fixed-Aside animation

--- js/app.js
+++ js/app.js
@@ -2,45 +2,36 @@ const moviesContainer = document.querySelector(".movies-container");
 const input = document.getElementById("input-field");
 const asideContainer = document.getElementById("aside-movie");
 const closeButton = document.getElementById("close-button");
+const pageContainer = document.querySelector(".page-container");
 
-
-const getMostRatedMovies = async () => {
+const getMovies = async (option, page = 1) => {
+  const language = `pt-BR`;
+  let url;
+  switch (option) {
+    case `Popular`:
+      url = `/movie/popular?language=${language}S&page=${page}`;
+      break;
+    case `Top Rated`:
+      url = `/movie/top_rated?language=${language}&page=${page}`;
+      break;
+    default:
+      url = `/movie/upcoming?language=en-US&page=1`;
+  }
   try {
-    const data = await fetchMovies(`/movie/top_rated?language=en-US&page=1`);
+    const data = await fetchMovies(url);
     for (const movieId of data.results) {
       getMovieData(movieId.id);
     }
   } catch (error) {
     console.log(error);
   }
+
 };
 
-const getMostPopularMovies = async () => {
-  try {
-    const data = await fetchMovies(`/movie/popula?language=en-US&page=1`);
-    for (const movieId of data.results) {
-      getMovieData(movieId.id);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getTrendingMovies = async () => {
-  try {
-    const data = await fetchMovies(`/trending/movie/day?language=en-US`);
-    console.log(data);
-    for (const movieId of data.results) {
-      getMovieData(movieId.id);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-window.addEventListener("load", () => getTrendingMovies());
+window.addEventListener("load", () => getMovies());
 
 const getMovieData = async (movieId) => {
+  moviesContainer.innerHTML = "";
   try {
     const data = await fetchMovies(`/movie/${movieId}?`);
     createCard(data);
@@ -57,24 +48,47 @@ const createCard = (data) => {
   card.style.border = "none";
   card.addEventListener("click", () => aside(data));
   card.innerHTML = `
-    <div>
-        <img src=${
-          poster_path
-            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : "https://static.vecteezy.com/system/resources/previews/005/337/799/non_2x/icon-image-not-found-free-vector.jpg"
-        } class="card-img-top" alt="${title} img">
+  <div class="card-container">
+  <img src=${
+    poster_path
+      ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+      : "https://static.vecteezy.com/system/resources/previews/005/337/799/non_2x/icon-image-not-found-free-vector.jpg"
+  } class="card-img-top" alt="${title} img">
       <div class="card-body">
         <h6>${title}</h6>
         <div class="movie-info"
-          <p>${vote_average.toFixed(1)}</p>
-          <p>${runtime ? runtime + " " + "min" : "runtime is undefined"}  </p>
+        <p>${vote_average === 0 ? "Not found" : vote_average.toFixed(1)}</p>
+        <p>${runtime ? runtime + " " + "min" : "runtime is undefined"}  </p>
         </div>
-      </div>
-    </div>
-  `;
+        </div>
+        </div>
+        
+        `;
   moviesContainer.appendChild(card);
-  moviesContainer.querySelector(".card");
+  card.style.opacity = "1";
 };
+
+let defaultPages = [1, 2, 3, 4, 5];
+let currentPage = [];
+
+const getCurrentPage = () => {
+  pageContainer.innerHTML = ""
+  setTimeout(() => {
+    let pages = document.createElement("div");
+    pages.innerHTML = `
+          <div class="btn-group">
+            ${
+            defaultPages.map((page) => {
+            return `<a class="btn btn-primary active">${page}</a>`
+          })
+            }
+          </div>
+          `;
+    pageContainer.appendChild(pages);
+  }, 350)
+};
+
+getCurrentPage()
 
 let typingTimeout;
 
@@ -87,18 +101,16 @@ input.addEventListener("keyup", () => {
       searchMovies(inputValue);
     } else {
       moviesContainer.innerHTML = "";
-      getTrendingMovies();
+      getMovies();
     }
-  }, 300); // Aguarda 300ms antes de realizar a busca
+  }, 300);
 });
 
 const aside = (data) => {
   if (asideContainer.querySelector(".aside-info")) {
     asideContainer.querySelector(".aside-info").remove();
   }
-  asideContainer.style.opacity = "1";
-  asideContainer.style.width = "550px";
-  asideContainer.style.right = "0vw";
+  asideContainer.style.transform = "translateX(0%)";
   setTimeout(() => {
     const { poster_path, title, vote_average, runtime } = data;
 
@@ -120,6 +132,7 @@ const aside = (data) => {
     </div>
     </div>
     `;
+
     asideContainer.appendChild(asideInfo);
   }, 100);
 };
@@ -127,11 +140,17 @@ const aside = (data) => {
 closeButton.addEventListener("click", () => closeAside());
 
 const closeAside = () => {
-  console.log("click");
+  asideContainer.style.transform = "translateX(100%)";
   asideContainer.querySelector(".aside-info").innerHTML = "";
-  asideContainer.style.opacity = "0";
-  asideContainer.style.width = "0";
-  asideContainer.style.right = "5vw";
 };
 
-document.querySelectorAll(".tag-container p").forEach()
+document.querySelectorAll(".tag-container p").forEach((option) =>
+  option.addEventListener("click", () => {
+    moviesContainer.innerHTML = "";
+    setTimeout(() => {
+      const optionName = option.textContent;
+      getMovies(optionName);
+      getCurrentPage()
+    }, 150);
+  })
+);
